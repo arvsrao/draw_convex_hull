0f755a895cf745447b395ad6e2a552ad645dcba4
Arvind Rao <arvind.rao@here.com>
2020-05-07 13:44:28 +0200

    conveniently format project with  pre-commit bash script

---
+++ pre-commit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if ! [ -x "$(command -v clang-format)" ]; then
+  echo 'Error: clang-format is NOT installed.' >&2
+  exit 1
+fi
+
+clang-format --verbose -i --style=file include/impl/*cpp
+clang-format --verbose -i --style=file include/*h
+clang-format --verbose -i --style=file src/*cpp
+clang-format --verbose -i --style=file test/*cpp
+
+exit 0;
