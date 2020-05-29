98a65e0d88fe6af127fa01f16c00284c9df6cc50
Arvind Rao <arvsrao@gmail.com>
2020-05-30 00:33:08 +0200

    more triangle tests

--- test/DirectedAcyclicGraphTests.cpp
+++ test/DirectedAcyclicGraphTests.cpp
@@ -21,12 +21,14 @@ TEST(DAG, CreationTest) {
 TEST(DAG, PointInTriangleTest) {
   Point inside_point  = Point(0, 0);
   Point outside_point = Point(4, 4);
-  Point edge_point_bc = Point(2, 2);   // on edge {bc}
   Point edge_point_ab = Point(-2, 0);  // on edge {ab}
+  Point edge_point_bc = Point(2, 2);   // on edge {bc}
+  Point edge_point_ac = Point(0, 2);   // on edge {ac}
 
   ASSERT_TRUE(triangle.containsPoint(inside_point));
   ASSERT_FALSE(triangle.containsPoint(outside_point));
 
   ASSERT_TRUE(triangle.containsPoint(edge_point_ab));
   ASSERT_TRUE(triangle.containsPoint(edge_point_bc));
+  ASSERT_TRUE(triangle.containsPoint(edge_point_ac));
 }
